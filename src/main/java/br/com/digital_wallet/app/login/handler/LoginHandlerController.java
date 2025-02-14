bcaf1ffe587c4f3a34ab4da58198cbebdd345dc5
Andre Parelho <parelhoneves.andre@gmail.com>
2025-02-13 21:28:46 -0300

    feat: exceptions

--- src/main/java/br/com/digital_wallet/app/login/handler/LoginHandlerController.java
+++ src/main/java/br/com/digital_wallet/app/login/handler/LoginHandlerController.java
@@ -1,22 +1,18 @@
-package br.com.digital_wallet.app.user.handler;
+package br.com.digital_wallet.app.login.handler;
 
 import br.com.digital_wallet.app.common.exception.ExceptionResponse;
-import br.com.digital_wallet.app.user.handler.exception.UserAlreadyExistsException;
+import br.com.digital_wallet.app.login.handler.exception.PasswordNotValidException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDate;
 
-@RestController
-@ControllerAdvice
-public class UserHandlerController extends ResponseEntityExceptionHandler {
-    @ExceptionHandler(UserAlreadyExistsException.class)
-    public final ResponseEntity<ExceptionResponse> handleUserAlreadyExistsException(Exception exception, WebRequest webRequest){
+public class LoginHandlerController extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(PasswordNotValidException.class)
+    public final ResponseEntity<ExceptionResponse> handlePasswordNotValidException(Exception exception, WebRequest webRequest){
         ExceptionResponse exceptionResponse = new ExceptionResponse(LocalDate.now(), exception.getMessage(), webRequest.getDescription(false));
         return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
     }
--- src/main/java/br/com/digital_wallet/app/user/handler/exception/UserNotFoundException.java
+++ src/main/java/br/com/digital_wallet/app/user/handler/exception/UserNotFoundException.java
@@ -1,10 +1,13 @@
-package br.com.digital_wallet.app.user.handler.exceptions;
+package br.com.digital_wallet.app.user.handler.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.NO_CONTENT)
+import java.io.Serial;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class UserNotFoundException extends RuntimeException {
+    @Serial
     private static final long serialVersionUID = 1L;
 
     public UserNotFoundException(String message) {
