ab4add1a1c72718b49270cb8e10f42e50888a4b7
Andre Parelho <parelhoneves.andre@gmail.com>
2025-02-13 21:28:15 -0300

    feat: security

---
+++ src/main/java/br/com/digital_wallet/app/security/SecurityConfig.java
@@ -0,0 +1,30 @@
+package br.com.digital_wallet.app.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static br.com.digital_wallet.app.login.constants.LoginConstants.LOGIN_ENDPOINT;
+import static br.com.digital_wallet.app.user.util.UserConstants.USER_ENDPOINT;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests(auth -> auth
+                    .requestMatchers(HttpMethod.POST, USER_ENDPOINT).permitAll()
+                    .requestMatchers(HttpMethod.POST, LOGIN_ENDPOINT).permitAll()
+                    .anyRequest().authenticated())
+                .csrf(AbstractHttpConfigurer::disable)
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        return httpSecurity.build();
+    }
+}
